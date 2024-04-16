fde389b975d68931d03e669767d0a8f53d817ee0
VSD10 <vsdh2004@gmail.com>
2024-04-16 20:06:41 +0530

    update

--- script.js
+++ script.js
@@ -85,6 +85,20 @@ function recordLeave() {
      // Call printAttendance after updating leave
 }
 
+function toggleHelp() {
+    const helpText = document.getElementById("help-text");
+    const leaveInput = document.getElementById("leave-input");
+    const onDutyInput = document.getElementById("on-duty-input");
+  
+    if (helpText.style.display === "none") {
+      helpText.style.display = "block";
+      leaveInput.style.display = "none";
+      onDutyInput.style.display = "none";
+    } else {
+      helpText.style.display = "none";
+    }
+  }
+  
 function recordOnDuty() {
     const input = document.getElementById("on-duty-input-field").value;
     const indices = input.split(" ").map(Number);
